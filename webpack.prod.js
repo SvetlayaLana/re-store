e16274ce074dc0accbc6df81700768d449282208
SvetlayaLana <amber.world9@gmail.com>
2020-05-21 20:48:53 +0300

    builded

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ const Visualizer = require('webpack-visualizer-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 const baseConfig = require("./webpack.config");
-const APP_DIR = path.resolve(__dirname, '..');
+const APP_DIR = path.resolve(__dirname, './');
 
 const prodConfiguration = env => {
   return merge([
